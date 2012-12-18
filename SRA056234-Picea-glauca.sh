00d898f4d7ed246a334737beff868fae97031d28
Sébastien Boisvert <sebhtml@bgq-fen1.scinet.p7.local>
2012-12-18 14:55:28 -0500

    Ready to start !
    Signed-off-by: Sébastien Boisvert <sebhtml@bgq-fen1.scinet.p7.local>

---
+++ SRA056234-Picea-glauca.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# @ job_name           = SRA056234-Picea-glauca-2012-12-18-1
+# @ job_type           = bluegene
+# @ comment            = "white spruce for the win !"
+# @ output             = $(job_name).$(Host).$(jobid).out
+# @ error              = $(job_name).$(Host).$(jobid).err
+# @ bg_size            = 512
+# @ wall_clock_limit   = 48:00:00
+# @ bg_connectivity    = Torus
+# @ queue 
+
+runjob --np 8192 --ranks-per-node=32 --cwd=$PWD \
+: /home/c/clumeq/sebhtml/software/ray/Last-Build/Ray SRA056234-Picea-glauca.conf
+
