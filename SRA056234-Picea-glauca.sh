48d6587bb17d6a62b957de1f7f3430e08b936fff
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-12-25 08:18:56 -0500

    Updated core number for Blue Gene/Q
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- SRA056234-Picea-glauca.sh
+++ SRA056234-Picea-glauca.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# @ job_name           = SRA056234-Picea-glauca-2012-12-18-2
+# @ job_name           = SRA056234-Picea-glauca-2012-12-22-14
 # @ job_type           = bluegene
 # @ comment            = "white spruce for the win !"
 # @ output             = $(job_name).$(Host).$(jobid).out
@@ -9,6 +9,10 @@
 # @ bg_connectivity    = Torus
 # @ queue 
 
-runjob --np 8192 --ranks-per-node=32 --cwd=$PWD \
+#  512 * 16 =  8192
+#  768 * 16 = 12288
+# 1024 * 16 = 16384
+
+runjob --np 4096 --ranks-per-node=8 --cwd=$PWD \
 : /home/c/clumeq/sebhtml/software/ray/Last-Build/Ray SRA056234-Picea-glauca.conf
 
